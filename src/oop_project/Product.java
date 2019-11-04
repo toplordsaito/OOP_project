7a5983cabf3cd93a49e4e62d75c9cb0686eac318
toplordsaito <42961261+toplordsaito@users.noreply.github.com>
2019-11-04 18:14:31 +0700

    test

---
+++ src/oop_project/Product.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop_project;
+
+/**
+ *
+ * @author waruwat
+ */
+public class Product {
+    private int id;
+    private String name;
+    private int count;
+
+    public Product(int id, String name, int count) {
+        this.id = id;
+        this.name = name;
+        this.count = count;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+    
+    
+}
